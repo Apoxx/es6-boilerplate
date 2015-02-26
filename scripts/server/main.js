aef5b9b745469f982de4d18c953b38a5799cbdbe
Anthony Camboni <anthony@camboni.be>
2015-02-26 22:14:38 +0100

    Use process.cwd() instead of appDir and add shared scripts folder

--- scripts/server/main.js
+++ scripts/server/main.js
@@ -1,18 +1,16 @@
-import path from 'path';
 import express from 'express';
 import jade from 'jade';
 import jadeBabel from 'jade-babel';
 
 jade = jadeBabel({}, jade);
 
-const appDir = path.dirname(require.main.filename);
 const port = Number(process.env.PORT || 8080);
 
 const app = express()
 .engine('jade', jade.__express)
 .set('view engine', 'jade')
-.set('views', `${appDir}/views`)
-.use(express.static(`${appDir}/public`))
+.set('views', `${process.cwd()}/views`)
+.use(express.static(`${process.cwd()}/public`))
 .get('/', (req, res) => res.render('index'))
 .listen(port, () => {
   (msg => console.log(`Hello from ${msg} ! Listening on port ${port}`))('Node');
